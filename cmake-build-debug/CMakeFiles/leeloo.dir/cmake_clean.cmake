9e32d3432fbbb5b422511ee3d9210e52182587d8
ilmir-karimov <index.js@yandex.ru>
2019-02-12 07:52:11 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/leeloo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leeloo.dir/main.cpp.o"
+  "leeloo.pdb"
+  "leeloo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leeloo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
