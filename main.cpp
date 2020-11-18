69a406acbf316bf2c33225a5b4fdacf5cbb039c3
Ilmir Karimov <Ilmir_Karimov@eprupetw05e4.dlink>
2020-11-19 01:05:22 +0400

    Slightly refacor main.cpp

--- main.cpp
+++ main.cpp
@@ -7,150 +7,147 @@ using namespace std;
 
 class Error
 {
-    string msg;
+  string msg;
 
 public:
-    Error(string message) : msg(message) {}
+  Error(string message) : msg(message) {}
 
-    string what()
-    {
-        return msg;
-    }
+  string what()
+  {
+    return msg;
+  }
 };
 
 class Token
 {
 public:
-    enum Kind
-    {
-        none = 0,
-        id,
-        num,
-        txt,
-        plus,
-        minus,
-        mul,
-        div,
-        mod,
-        exclimation,
-        question,
-        colon,
-        semicolon,
-        eos,
-        eof,
-        newline,
-        r_par,
-        l_par,
-        l_cur,
-        r_cur,
-        l_ang,
-        r_ang
-    };
-    Kind kind = none;
-    double value = 0.0;
-    string text = "";
-    Token() {}
+  enum Kind
+  {
+    none = 0,
+    id,
+    num,
+    str,
+    plus,
+    minus,
+    asterisk,
+    div,
+    mod,
+    exclimation_mark,
+    question_mark,
+    colon,
+    semicolon,
+    bracket_round_left,
+    bracket_round_right,
+    bracket_curly_left,
+    bracket_curly_right,
+    bracket_angle_left,
+    bracket_angle_right,
+    end_of_string,
+    end_of_line,
+    end_of_file
+  };
+  Kind kind = none;
+  double value = 0.0;
+  string text = "";
 
-    friend ostream &operator<<(ostream &os, const Token &t);
+  Token() {}
+  Token(double d) : value(d), kind(num) {}
+  Token(Kind k) : kind(k) {}
+  Token(string t) : text(t), kind(str) {}
 
-    Token(double d) : value(d), kind(num) {}
-
-    Token(Kind k) : kind(k) {}
-
-    Token(string t) : text(t), kind(txt) {}
+  friend ostream &operator<<(ostream &out_stream, const Token &t);
 };
 
-ostream &operator<<(ostream &os, const Token &t)
+ostream &operator<<(ostream &out, const Token &t)
 {
-    switch (t.kind)
-    {
-    case Token::none:
-        os << "<none>";
-        break;
-    case Token::plus:
-        os << "+";
-        break;
-    case Token::minus:
-        os << "-";
-        break;
-    case Token::div:
-        os << "/";
-        break;
-    case Token::mul:
-        os << "*";
-        break;
-    case Token::mod:
-        os << "%";
-        break;
-    default:
-        os << "Some lexeme...";
-    }
-    return os;
+  switch (t.kind)
+  {
+  case Token::none:
+    out << "<none>";
+    break;
+  case Token::plus:
+    out << "+";
+    break;
+  case Token::minus:
+    out << "-";
+    break;
+  case Token::div:
+    out << "/";
+    break;
+  case Token::asterisk:
+    out << "*";
+    break;
+  case Token::mod:
+    out << "%";
+    break;
+  default:
+    out << "Some lexeme...";
+  }
+  return out;
 }
 
 class TokenStream
 {
-    vector<Token> buffer;
+  vector<Token> buffer;
 
 public:
-    void putback(Token &t) { buffer.push_back(t); }
+  void putback(Token &t) { buffer.push_back(t); }
 
-    Token get()
+  Token get()
+  {
+    if (buffer.size() > 0)
     {
-        if (buffer.size() > 0)
-        {
-            Token t = buffer.back();
-            buffer.pop_back();
-            return t;
-        }
-
-        char ch;
-        cin.get(ch);
-
-        switch (ch)
-        {
-
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-        case '.':
-        {
-            cin.putback(ch);
-            double d;
-            cin >> d;
-            return Token(d);
-        }
-        case '+':
-            return Token(Token::plus);
-        case '-':
-            return Token(Token::minus);
-        case '/':
-            return Token(Token::div);
-        case '*':
-            return Token(Token::mul);
-        case '%':
-            return Token(Token::mod);
-        case '(':
-            return Token(Token::l_par);
-        case ')':
-            return Token(Token::r_par);
-        case ';':
-            return Token(Token::semicolon);
-        case '\n':
-            return Token(Token::newline);
-        case '\0':
-            return Token(Token::eos);
-        default:
-            throw Error("Lexer error: Unknown lexeme.");
-        }
+      Token t = buffer.back();
+      buffer.pop_back();
+      return t;
     }
+
+    char ch;
+    cin.get(ch);
+
+    switch (ch)
+    {
+    case '0':
+    case '1':
+    case '2':
+    case '3':
+    case '4':
+    case '5':
+    case '6':
+    case '7':
+    case '8':
+    case '9':
+    case '.':
+    {
+      cin.putback(ch);
+      double d;
+      cin >> d;
+      return Token(d);
+    }
+    case '+':
+      return Token(Token::plus);
+    case '-':
+      return Token(Token::minus);
+    case '/':
+      return Token(Token::div);
+    case '*':
+      return Token(Token::asterisk);
+    case '%':
+      return Token(Token::mod);
+    case '(':
+      return Token(Token::bracket_round_left);
+    case ')':
+      return Token(Token::bracket_round_right);
+    case ';':
+      return Token(Token::semicolon);
+    case '\n':
+      return Token(Token::end_of_line);
+    case '\0':
+      return Token(Token::end_of_string);
+    default:
+      throw Error("Lexer error: Unknown lexeme.");
+    }
+  }
 };
 
 TokenStream ts;
@@ -159,114 +156,113 @@ double expression();
 
 double primary()
 {
-    Token t = ts.get();
+  Token t = ts.get();
 
-    switch (t.kind)
-    {
-    case Token::num:
-        return t.value;
-    case Token::plus:
-        return +primary();
-    case Token::minus:
-        return -primary();
-    case Token::l_par:
-    {
-        double exp = expression();
-        Token rb = ts.get();
-        if (rb.kind != Token::r_par)
-            throw Error("Primary error: closing round bracket expected.");
-        return exp;
-    }
-    default:
-        throw Error("Primary error: " + t.kind);
-    }
+  switch (t.kind)
+  {
+  case Token::num:
+    return t.value;
+  case Token::plus:
+    return +primary();
+  case Token::minus:
+    return -primary();
+  case Token::bracket_round_left:
+  {
+    double expr = expression();
+    Token rb = ts.get();
+    if (rb.kind != Token::bracket_round_right)
+      throw Error("Primary error: closing round bracket expected.");
+    return expr;
+  }
+  default:
+    throw Error("Primary error: " + t.kind);
+  }
 }
 
 double term()
 {
-    double left = primary();
-    Token t = ts.get();
+  double left = primary();
+  Token t = ts.get();
 
-    while (true)
-        switch (t.kind)
-        {
-        case Token::mul:
-            left *= primary();
-            t = ts.get();
-            break;
+  while (true)
+    switch (t.kind)
+    {
+    case Token::asterisk:
+      left *= primary();
+      t = ts.get();
+      break;
 
-        case Token::div:
-        {
-            double right = primary();
-            if (right == 0)
-            {
-                return numeric_limits<double>::infinity();
-                //throw Error("Division by zero.");
-            }
-            else
-            {
-                left /= right;
-                t = ts.get();
-                break;
-            }
-        }
+    case Token::div:
+    {
+      double right = primary();
+      if (right == 0)
+      {
+        return numeric_limits<double>::infinity();
+      }
+      else
+      {
+        left /= right;
+        t = ts.get();
+        break;
+      }
+    }
 
-        default:
-            ts.putback(t);
-            return left;
-        }
+    default:
+      ts.putback(t);
+      return left;
+    }
 }
 
 double expression()
 {
-    double left = term();
-    Token t = ts.get();
+  double left = term();
+  Token t = ts.get();
 
-    while (true)
-        switch (t.kind)
-        {
-        case Token::plus:
-            left += term();
-            t = ts.get();
-            break;
+  while (true)
+    switch (t.kind)
+    {
+    case Token::plus:
+      left += term();
+      t = ts.get();
+      break;
 
-        case Token::minus:
-            left -= term();
-            t = ts.get();
-            break;
+    case Token::minus:
+      left -= term();
+      t = ts.get();
+      break;
 
-        default:
-            ts.putback(t);
-            return left;
-        }
+    default:
+      ts.putback(t);
+      return left;
+    }
 }
 
 int main()
 {
 
-    cout << "> ";
-    while (true)
-        try
-        {
+  cout << "> ";
+  while (true)
+    try
+    {
 
-            Token t = ts.get();
-            while (t.kind == Token::semicolon || t.kind == Token::newline || t.kind == Token::eos)
-            {
-                t = ts.get();
-            }
+      Token t = ts.get();
+      while (t.kind == Token::semicolon || t.kind == Token::end_of_line || t.kind == Token::end_of_string)
+      {
+        t = ts.get();
+      }
 
-            ts.putback(t);
+      ts.putback(t);
 
-            double res = expression();
+      double res = expression();
 
-            cout << ": " << res << endl;
-            cout << "> ";
-        }
-        catch (Error &e)
-        {
-            cerr << endl
-                 << "Catch: " << e.what() << endl;
-            break;
-        };
-    return 0;
+      cout << ": " << res << endl;
+      cout << "> ";
+    }
+    catch (Error &e)
+    {
+      cerr << endl
+           << "Catch: " << e.what() << endl;
+      break;
+    };
+  return 0;
 }
